a2eb8b6a0ca22452efafdbe881ee7bea94431861
Robby Divine <rdivine48@gmail.com>
2021-11-19 09:30:09 -0800

    updating enumerate_permutations to a much better approach

--- rosalind_exercises/enumerate_permutations.py
+++ rosalind_exercises/enumerate_permutations.py
@@ -1,21 +1,10 @@
-num = 6
+import itertools as it
 
-def dupcheck(x):
-    for elem in x:
-        if x.count(elem) > 1:
-            return True
-    return False
+n = 6
 
-total = 0
+def permutations(i):
+    # takes an int, returns a list of tuples
+    p = it.permutations(range(1 , i+1))
+    return [j for j in p]
 
-for i in range(1,num+1):
-    for j in range(1,num+1):
-        for k in range(1,num+1):
-            for l in range(1,num+1):
-                for m in range(1,num+1):
-                    for n in range(1,num+1):
-                        if not dupcheck([i, j, k, l, m, n]):
-                            total += 1
-                            print(i, j, k, l, m, n)
-
-print(total)
+print(permutations(n))
