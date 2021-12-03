40b16cf72952e16d191ce7155cc404d143287f0a
rdd48 <rdivine48@gmail.com>
2021-12-02 21:26:43 -0800

    cleaning up longest palindrome by adding functions

--- leetcode/longest_palindromic_substr.py
+++ leetcode/longest_palindromic_substr.py
@@ -1,3 +1,16 @@
+def searchPalindrome(low_i, high_i, s):
+    while low_i >= 0 and high_i < len(s):
+        if s[low_i] == s[high_i]:
+            low_i -= 1
+            high_i += 1
+        else:
+            break
+        
+    low_i += 1
+    high_i -= 1
+
+    return low_i, high_i
+
 def longestPalindrome(s: str) -> str:
 
     if len(s) == 1:
@@ -12,40 +25,19 @@ def longestPalindrome(s: str) -> str:
     best_low, best_high = 0, 0
 
     for i, ch in enumerate(s):
-        low_i = i
-        high_i = i + 1
 
-        # adjust high_i for case where palindrome len is even
-        while low_i >= 0 and high_i < len(s):
-            if s[low_i] == s[high_i]:
-                low_i -= 1
-                high_i += 1
-            else:
-                break
-        
-        low_i += 1
-        high_i -= 1
+        # case when palindrome len is even
+        low_i, high_i = searchPalindrome(i, i+1, s)
 
         curr_score = high_i - low_i
         if curr_score > best_score:
             best_score = curr_score
             best_low = low_i
             best_high = high_i
-
-        low_i = i - 1
-        high_i = i + 1
-
-        while low_i >= 0 and high_i < len(s):
-            if s[low_i] == s[high_i]:
-                low_i -= 1
-                high_i += 1
-            else:
-                break
         
-        # readjust the i's
-        low_i += 1
-        high_i -= 1
-
+        # case when palindrome len is odd
+        low_i, high_i = searchPalindrome(i-1, i+1, s)
+       
         curr_score = high_i - low_i
         if curr_score > best_score:
             best_score = curr_score
@@ -54,4 +46,7 @@ def longestPalindrome(s: str) -> str:
     
     return s[best_low:best_high+1]
 
-print(longestPalindrome('cc'))
+print(longestPalindrome('ccc'))
+print(longestPalindrome('cccc'))
+print(longestPalindrome('abc'))
+print(longestPalindrome('dabccbae'))
