042b193c1873ce60355e7762f3b35978062ee2da
Robby Divine <rdivine48@gmail.com>
2022-07-21 12:47:17 -0700

    adding defaults

--- streamlit/pymol_visualizer/pymol_visualizer.py
+++ streamlit/pymol_visualizer/pymol_visualizer.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import py3Dmol
 from stmol import showmol
+from utils import pdb_parser
+import re
+from urllib import request
+import gzip
+
+# TODO:
+# * select by resi
+
 
 st.set_page_config(
     page_title='Pymol Visualizer',
@@ -8,49 +16,82 @@ st.set_page_config(
     layout='wide'
 )
 
-st.markdown('#### :atom_symbol:  Pymol Visualizer  :atom_symbol:')
-mobile_size = st.checkbox('Optimize for mobile?', value=False)
-
-# how to use py3Dmol: https://william-dawson.github.io/using-py3dmol.html
-# with open('1fsl.pdb') as pdb:
-#     system = ''.join([i for i in pdb])
+# st.markdown('#### :atom_symbol:  Pymol Visualizer  :atom_symbol:')
+mobile_size = st.checkbox('Optimize size for mobile?', value=False)
 
 # globals
 width = 250 if mobile_size else 600
 height = 250 if mobile_size else 400
 
+col1, col2 = st.columns([1,2])
+with col1:
+    pdb = st.text_input('Enter PDB code:')
+    pdb = pdb.strip()
+
+with col2:
+    selected_pdb = st.selectbox('Or pick from defaults:', options=('(None)', 'LegH', 'Soy 11S', 'Soy 7S'))
+    if selected_pdb == 'LegH':
+        pdb = '1FSL'
+    elif selected_pdb == 'Soy 11S':
+        pdb = '1OD5'
+    elif selected_pdb == 'Soy 7S':
+        pdb = '1UIK'
+
+if pdb:
+    url1 = f'https://files.rcsb.org/download/{pdb}.pdb1.gz'
+    file_name1 = re.split(pattern='/', string=url1)[-1]
+    r1 = request.urlretrieve(url=url1, filename=file_name1)
+    txt1 = re.split(pattern=r'\.', string=file_name1)[0] + '.txt'
+
+    with gzip.open(file_name1, 'rt') as f_in:
+        bio_assembly = f_in.readlines()
+        parsed_pdb = ''
+        for line in bio_assembly:
+            if line.startswith('ATOM'):
+                parsed_pdb += line
+
+    view = py3Dmol.view(width=width, height=height)
+    view.addModelsAsFrames(parsed_pdb)
+    view.setStyle({'model': -1}, {'cartoon': {'color': 'spectrum'}})
+
+    # display pdb element title
+    st.markdown(f'**[{pdb.upper()}](https://www.rcsb.org/structure/{pdb.upper()}): {pdb_parser.get_name(pdb)}**')
+
+    chains, sequence = pdb_parser.parse_pdb(bio_assembly)
+
 st.sidebar.header('Select options:')
-pdb = st.sidebar.text_input('Enter PDB code:', value='1fsl')
-
-color_selector = st.sidebar.selectbox('Select color from picker or from pymol defaults:', ('Spectrum', 'Color Picker', 'cbc'))
-if color_selector == 'Color Picker':
-    color = st.sidebar.color_picker('Select color from picker:', value='#00f900')
-elif color_selector:
-    color = color_selector.lower()
-
+color_selector = st.sidebar.selectbox('Select color from picker or from pymol defaults:', ('Spectrum', 'Color by Chain', 'Color Picker',))
 spinning = st.sidebar.selectbox('Set model spin', ('False', 'True'))
 
-view = py3Dmol.view(query=f'pdb:{pdb.strip()}', width=width, height=height)
-view.setStyle({'model': -1}, {'cartoon': {'color': color}})
+if pdb:
+    if color_selector == 'Color Picker':
+        color = st.sidebar.color_picker('Select color from picker:', value='#00f900')
+        view.setStyle({'model': -1}, {'cartoon': {'color': color}})
+    elif color_selector == 'Color by Chain':
+        colors = ['green', 'cyan', 'magenta', 'yellow', 'wheat', 'slate', 'grey', 'lightpink', 'blue', 'red']
+        for idx, c in enumerate(chains):
+            view.setStyle({'chain': c}, {'cartoon': {'color': colors[idx]}})
+    elif color_selector:
+        color = color_selector.lower()
+        view.setStyle({'model': -1}, {'cartoon': {'color': color}})
 
-if spinning == 'True':
-    spin_axis = st.sidebar.selectbox('Which axis?', ('x', 'y', 'z'))
-    speed = st.sidebar.slider('How fast? (Default is 1)', min_value = 0.5, max_value=3., value=1., step=0.5)
-    spin_reverse = st.sidebar.selectbox('Reverse?', ('False', 'True'))
-    spin_direction = 1.0 if spin_reverse == 'False' else -1.0
-    view.spin(spin_axis, speed * spin_direction)
-    # st.markdown(f'{speed}, {speed*spin_direction}, {spin_direction}')
+    if spinning == 'True':
+        spin_axis = st.sidebar.selectbox('Which axis?', ('x', 'y', 'z'))
+        speed = st.sidebar.slider('How fast? (Default is 1)', min_value = 0.5, max_value=3., value=1., step=0.5)
+        spin_reverse = st.sidebar.selectbox('Reverse?', ('False', 'True'))
+        spin_direction = 1.0 if spin_reverse == 'False' else -1.0
+        view.spin(spin_axis, speed * spin_direction)
 
-button = st.sidebar.button('Reset view', on_click=view.zoomTo)
+    button = st.sidebar.button('Reset view', on_click=view.zoomTo)
 
 # center the title
 st.markdown('<style>.e16nr0p30 {text-align: center;}</style>', unsafe_allow_html=True)
 
 # adjust the view size and add a border
-st.markdown('<style>iframe {border: solid; display: block; margin-left: auto; margin-right: auto;}</style>', unsafe_allow_html=True)
+st.markdown('<style>iframe {border: solid; display: block; margin-left: auto; margin-right: auto; padding-left: 0px;}</style>', unsafe_allow_html=True)
 
-view.zoomTo()
-view.show()
+if pdb:
+    view.zoomTo()
+    view.show()
 
-
-showmol(view, width=width, height=height)
+    showmol(view, width=width, height=height)
---
+++ streamlit/pymol_visualizer/utils/pdb_parser.py
@@ -0,0 +1,46 @@
+from pypdb import get_info
+
+aa_codes = {
+    'ALA': 'A', 'CYS': 'C', 'ASP': 'D', 'GLU': 'E',
+    'PHE': 'F', 'GLY': 'G', 'HIS': 'H', 'ILE': 'I', 
+    'LYS': 'K', 'LEU': 'L', 'MET': 'M', 'ASN': 'N',
+    'PRO': 'P', 'GLN': 'Q', 'ARG': 'R', 'SER': 'S',
+    'THR': 'T', 'VAL': 'V', 'TRP': 'W', 'TYR': 'Y'
+} 
+
+def get_name(pdb):
+    pdb_data = get_info(pdb)
+    return pdb_data['struct']['title'].title()
+
+def get_rcsb_info(pdb):
+    pdb_data = get_info(pdb)
+    return pdb_data
+
+class PDBInfo:
+    def __init__(self, pdb):
+        self.pdb = pdb
+
+        self.chains = []
+        self.sequence = ''
+
+        for line in pdb:
+            if line.startswith('ATOM'):
+                if line[21] not in self.chains:
+                    self.chains.append(line[21])
+                if line[13:15] == 'N ':
+                    self.sequence += aa_codes[line[17:20]]
+    
+    # def get_chains(self):
+    #     return self.chains
+    
+    # def get_sequence(self):
+    #     return self.sequence
+
+
+
+def parse_pdb(pdb):
+    '''function to get all pdb info manually. get_pdb_file returns a string, hence all the splitting.'''
+    Pdb = PDBInfo(pdb)
+    return Pdb.chains, Pdb.sequence
+
+    
