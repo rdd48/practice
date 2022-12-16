b284d13aa55dfd5ad082945c632ea2a51f43a0b4
Robby Divine <rddivine@ucdavis.edu>
2022-12-15 17:10:49 -0800

    day 15 solution, oof though

--- adventofcode/2022/15_beacons.py
+++ adventofcode/2022/15_beacons.py
@@ -1,3 +1,6 @@
+# huge thanks to https://github.com/tipa16384/adventofcode/blob/main/2022/puzzle15.py
+# otherwise i wasn't getting very close
+
 def calc_man_dist(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
@@ -50,7 +53,6 @@ def possible_beacons1(fname):
             curr_dist = calc_man_dist(s, (x, y))
             if curr_dist <= md:
                 if (x, y) not in beacons_at_row:
-                    # print(x)
                     possible_beacons += 1
                     break
         
@@ -58,5 +60,74 @@ def possible_beacons1(fname):
     return possible_beacons
 
 
-print(possible_beacons1('input/test.txt'))
+# print(possible_beacons1('input/test.txt'))
 print(possible_beacons1('input/15.txt'))
+
+
+def add_point_to_set(s, x, y):
+    if x >= 0 and x <= 4000000 and y >= 0 and y <= 4000000:
+        s.add((x, y))
+    return s
+
+def is_inside(sensors, man_dists, x, y):
+    for s, md in zip(sensors, man_dists):
+        sx, sy = s
+        if calc_man_dist((sx, sy), (x, y)) <= md:
+            return True
+    return False
+
+def possible_beacons2(fname):
+    with open(fname) as f:
+        lines = f.readlines()
+        lines = [l.strip() for l in lines]
+
+        sensors, beacons = [], []
+
+        for l in lines:
+            l = l.replace('Sensor at ','').replace(' closest beacon is at ','')
+            p1, p2 = l.split(':')
+            sensors.append(inp_str_to_ints(p1))
+            beacons.append(inp_str_to_ints(p2))
+    
+    # get man distance for each sensor / beacon pair
+    man_dists = []
+    all_possibles = {}
+    
+    for s, b in zip(sensors, beacons):
+        man_dists.append(calc_man_dist(s, b))
+    
+        
+    idx = 0 
+    for s, md in zip(sensors, man_dists):
+        # get possible points for all sensor
+        print(idx)
+        possibles = set()
+        sx, sy = s
+        idx += 1
+
+        for d in range(md+2):
+            possibles = add_point_to_set(possibles, sx - md - 1 + d, sy + d)
+            possibles = add_point_to_set(possibles, sx + md + 1 - d, sy + d)
+            possibles = add_point_to_set(possibles, sx - md - 1 + d, sy - d)
+            possibles = add_point_to_set(possibles, sx + md + 1 - d, sy - d)
+    
+        all_possibles[(sx, sy)] = possibles
+
+    ans = {}
+    for v in all_possibles.values():
+        for p in v:
+            x, y = p
+            if (x,y) not in ans:
+                ans[(x,y)] = 0
+            ans[(x,y)] += 1
+            
+            # this is the clever part: the only free part is one where the added buffer 
+            # from each diamond is found in (at least) 4 different sensors
+            if ans[(x,y)] >= 4:
+
+                # make sure the diamond is not inside other sensors diamonds
+                if not is_inside(sensors, man_dists, x, y):
+                    return x * 4000000 + y
+
+print(possible_beacons2('input/test.txt'))
+print(possible_beacons2('input/15.txt'))
