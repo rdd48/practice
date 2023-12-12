56620188156c48ac0eba25793947c324821ac95c
Robby Divine <rddivine@ucdavis.edu>
2023-12-12 11:17:34 -0800

    day 7 2023 aoc

--- adventofcode/2023/7.py
+++ adventofcode/2023/7.py
@@ -1,45 +1,25 @@
 def is5pair(h):
-    for i in h:
-        if h.count(i) == 5:
-            return i*5
-    return False
+    return True if h.count(max(h, key=h.count)) == 5 else False
 def is4pair(h):
-    for i in h:
-        if h.count(i) == 4:
-            return (i*4) + h.replace(i,'')
-    return False
+    return True if h.count(max(h, key=h.count)) == 4 else False
 def isfullhouse(h):
     for i in h:
         if h.count(i) == 3 and h.count(h.replace(i, '')[0]) == 2:
-            return (i*3) + h.replace(i, '')
+            return True
     return False
 def is3pair(h):
-    for i in h:
-        if h.count(i) == 3:
-            remain = h.replace(i, '')
-            rc = ''.join(sorted(remain, key=lambda x:r.index(x)))
-            return (i*3) + rc
-    return False
+    return True if h.count(max(h, key=h.count)) == 3 else False
 def is2pair(h):
     for i in h:
         if h.count(i) == 2:
             for j in h.replace(i, ''):
                 if h.count(j) == 2:
-                    if r.index(i) > r.index(j):
-                        return (i * 2) + (j * 2) + h.replace(i,'').replace(j,'')
-                    elif r.index(i) < r.index(j):
-                        return (j * 2) + (i * 2) + h.replace(i,'').replace(j,'')
+                    return h 
     return False
 def is1pair(h):
-    for i in h:
-        if h.count(i) == 2:
-            remain = h.replace(i, '')
-            rc = ''.join(sorted(remain, key=lambda x:r.index(x)))
-            return (i*2) + rc
-    return False
+    return True if h.count(max(h, key=h.count)) == 2 else False
 
 def part1(fname):
-    global r 
     r = 'AKQJT98765432'
     with open(fname) as f:
         lines = f.readlines()
@@ -48,53 +28,91 @@ def part1(fname):
     
     pair5s, pair4s, fhs, pair3s, pair2s, pair1s, hicards = [], [], [], [], [], [], []
     for h, b in zip(hands, bids):
-        if x := is5pair(h):
-            pair5s.append((x, b))
-        elif x := is4pair(h):
-            pair4s.append((x, b))
-        elif x := isfullhouse(h):
-            fhs.append((x, b))
-        elif x := is3pair(h):
-            pair3s.append((x, b))
-        elif x := is2pair(h):
-            pair2s.append((x, b))
-        elif x := is1pair(h):
-            pair1s.append((x, b))
+        if is5pair(h):
+            pair5s.append((h, b))
+        elif is4pair(h):
+            pair4s.append((h, b))
+        elif isfullhouse(h):
+            fhs.append((h, b))
+        elif is3pair(h):
+            pair3s.append((h, b))
+        elif is2pair(h):
+            pair2s.append((h, b))
+        elif is1pair(h):
+            pair1s.append((h, b))
         else:
-            hicards.append((''.join(sorted(h, key=lambda x:r.index(x))), b))
-    
-    pair5s.sort(key=lambda x:r.index(x[0][0]))
-    pair4s.sort(key=lambda word: [r.index(c) for c in word[0]])
-    fhs.sort(key=lambda word: [r.index(c) for c in word[0]])
-    pair3s.sort(key=lambda word: [r.index(c) for c in word[0]])
-    pair2s.sort(key=lambda word: [r.index(c) for c in word[0]])
-    pair1s.sort(key=lambda word: [r.index(c) for c in word[0]])
-    hicards.sort(key=lambda word: [r.index(c) for c in word[0]])
+            hicards.append((h, b))
 
     rank = len(hands)
     total = 0
     all_hands = [pair5s, pair4s, fhs, pair3s, pair2s, pair1s, hicards]
     for ah in all_hands:
+        ah.sort(key=lambda word: [r.index(c) for c in word[0]])
         for h in ah:
-            # print(h)
             total += rank * h[1]
             rank -= 1
 
     return total
-    
-print(part1('input/test.txt'))
-# print(part1('input/7.txt'))
 
 
+def part2(fname):
+    r2 = 'AKQT98765432J'
+
+    with open(fname) as f:
+        lines = f.readlines()
+        hands = [l.strip().split()[0] for l in lines if l.strip()]
+        bids = [int(l.strip().split()[1]) for l in lines if l.strip()]
     
-# pair5s, pair4s, fhs = [], {k:'' for k in r}, {k:'' for k in r}
-#     pair3s, pair2s, pair1s, hicards = {k:{i:'' for i in r} for k in r}, [], [], []
-#     for h, b in zip(hands, bids):
-#         if x := is5pair(h):
-#             pair5s.append((x, b))
-#         elif x := is4pair(h):
-#             pair4s[x[0]] = (x[1], b)
-#         elif x := isfullhouse(h):
-#             fhs[x[0]] = (x[-1], b)
-#         elif x := is3pair(h):
-#             pair3s[x[0]] = {x[-2]: x[-1]}
+    pair5s, pair4s, fhs, pair3s, pair2s, pair1s, hicards = [], [], [], [], [], [], []
+    for h, b in zip(hands, bids):
+        if 'J' not in h:
+            if is5pair(h):
+                pair5s.append((h, b))
+            elif is4pair(h):
+                pair4s.append((h, b))
+            elif isfullhouse(h):
+                fhs.append((h, b))
+            elif is3pair(h):
+                pair3s.append((h, b))
+            elif is2pair(h):
+                pair2s.append((h, b))
+            elif is1pair(h):
+                pair1s.append((h, b))
+            else:
+                hicards.append((h, b))
+        else:
+            if h.count('J') == 5:
+                pair5s.append((h, b))
+            elif h.count('J') + h.count(max(h.replace('J',''), key=h.count, default='')) == 5:
+                pair5s.append((h, b))
+            elif h.count('J') + h.count(max(h.replace('J',''), key=h.count, default='')) == 4:
+                pair4s.append((h, b))
+            elif h.count('J') == 1 and is2pair(h.replace('J','')): # full house case
+                fhs.append((h, b))
+            elif h.count('J') + h.count(max(h.replace('J',''), key=h.count, default='')) == 3:
+                pair3s.append((h, b))
+            elif h.count('J') == 1 and is1pair(h.replace('J','')):
+                pair2s.append((h, b))
+            elif h.count('J') + h.count(max(h.replace('J',''), key=h.count, default='')) == 2:
+                pair1s.append((h, b))
+            else:
+                print(h)
+                exit()
+
+    
+    rank = len(hands)
+    total = 0
+    all_hands = [pair5s, pair4s, fhs, pair3s, pair2s, pair1s, hicards]
+    for ah in all_hands:
+        ah.sort(key=lambda word: [r2.index(c) for c in word[0]])
+        for h in ah:
+            total += rank * h[1]
+            rank -= 1
+
+    return total
+
+# print(part1('input/test.txt'))
+print(part1('input/7.txt'))
+# print(part2('input/test.txt'))
+print(part2('input/7.txt'))
+    
